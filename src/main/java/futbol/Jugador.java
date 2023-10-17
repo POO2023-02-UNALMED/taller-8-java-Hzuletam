1caab45d03eec4d79375172773f3cadef1d315f0
Hzuletam <142773639+Hzuletam@users.noreply.github.com>
2023-10-17 11:57:43 -0500

    h

--- src/main/java/futbol/Jugador.java
+++ src/main/java/futbol/Jugador.java
@@ -40,7 +40,7 @@ public class Jugador extends Futbolista{
 
     @Override
     public String toString(){
-        return ("El futbolista " + this.getNombre() + " tiene " + this.getEdad() + " y juega de " + this.getPosicion() + " con el dorsal " + this.dorsal + ". Ha marcado " + this.golesMarcados);
+        return ("El futbolista " + this.getNombre() + " tiene " + this.getEdad() + ", y juega de " + this.getPosicion() + " con el dorsal " + this.dorsal + ". Ha marcado " + this.golesMarcados);
     }
 
     @Override
